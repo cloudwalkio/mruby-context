a1afe71c394db35389601dded24e6b4b5806cd37
Thiago Scalone <thiago@scalone.com.br>
2016-01-06 16:27:15 -0200

    Posxml startup.

--- mrblib/context.rb
+++ mrblib/context.rb
@@ -9,38 +9,54 @@ class Context
 
   def self.start(app = "main", platform = nil, json = nil)
     begin
-      $LOAD_PATH = ["./#{app}"]
-
-      # Library responsable for common code and API syntax for the user
-      if File.exist?("./#{app}/da_funk.mrb")
-        require "da_funk.mrb"
+      if app.split(".")[1] == "posxml"
+        posxml(app, platform)
       else
-        require "./main/da_funk.mrb"
-      end
-
-      # Platform library responsible for implement the adapter for DaFunk
-      # class Device #DaFunk abstraction
-      #   self.adapter =
-      platform_mrb = "./main/#{platform.to_s.downcase}.mrb"
-      if platform && File.exist?(platform_mrb)
-        require platform_mrb
-        Device::Support.constantize(platform).setup
-      else
-        # TODO
-        # DaFunk.setup_command_line
-      end
-
-      Device::System.klass = app if require "main.mrb"
-
-      # Main should have implement method call
-      #  method call was need to avoid memory leak on irep table
-      if json.nil? || json.empty?
-        Main.call
-      else
-        Main.call(json)
+        ruby(app, platform, json)
       end
     rescue => @exception
       self.treat(@exception)
+    end
+  end
+
+  def self.posxml(file, platform)
+    self.setup(file, platform)
+    Device::System.klass = file
+    PosxmlInterpreter.new(file).start
+  end
+
+  def self.ruby(app, platform, json)
+    $LOAD_PATH = ["./#{app}"]
+    self.setup(app, platform)
+    Device::System.klass = app if require "main.mrb"
+
+    # Main should have implement method call
+    #  method call was need to avoid memory leak on irep table
+    if json.nil? || json.empty?
+      Main.call
+    else
+      Main.call(json)
+    end
+  end
+
+  def self.setup(app, platform)
+    # Library responsable for common code and API syntax for the user
+    if File.exist?("./#{app}/da_funk.mrb")
+      require "da_funk.mrb"
+    else
+      require "./main/da_funk.mrb"
+    end
+
+    # Platform library responsible for implement the adapter for DaFunk
+    # class Device #DaFunk abstraction
+    #   self.adapter =
+    platform_mrb = "./main/#{platform.to_s.downcase}.mrb"
+    if platform && File.exist?(platform_mrb)
+      require platform_mrb
+      Device::Support.constantize(platform).setup
+    else
+      # TODO
+      # DaFunk.setup_command_line
     end
   end
 

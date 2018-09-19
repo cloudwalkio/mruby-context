cd8dc6466411e61845256a5ba88dcd055a30a549
William Roberto <william.roberto.t@gmail.com>
2018-09-19 12:13:52 -0300

    Just changed some methods to be private.

--- mrblib/json_log.rb
+++ mrblib/json_log.rb
@@ -3,19 +3,21 @@ class JsonLog
     :connections => Array.new
   }
 
-  def log_txt_to_json(logname)
+  def self.log_txt_to_json(logname)
     array = []
     File.read(logname).each_line do |line|
       array.push(line)
       if (line.index('D') && line.index("finished")) || (line.index('T') && line.index("WebSocket Recv Size")) ||
       line.index("ERROR") || line.index("WARN") || line.index("[EX] BACKTRACE")
-        hash[:connections].push(self.parse_log(array))
+        hash[:connections].push(parse_log(array))
         array = []
       end
     end
   end
 
-  def self.parse_log(array)
+  private
+
+  def parse_log(array)
     details = {
       :type => " ",
       :events => Array.new
@@ -23,9 +25,9 @@ class JsonLog
     array.each do |str|
       hash = {}
 
-      timestamp = self.get_timestamp(str)
+      timestamp = get_timestamp(str)
       event = str[(str.index('[') + 1)..-3]
-      details[:type] = self.get_type_transaction(event)
+      details[:type] = get_type_transaction(event)
 
       hash[:timestamp] = timestamp
       hash[:event] = event
@@ -34,7 +36,7 @@ class JsonLog
     details
   end
 
-  def self.get_type_transaction(event)
+  def get_type_transaction(event)
     if event.index('[D]')
       return "download"
     elsif event.index('[F]') || event.index('[T]')
@@ -46,7 +48,7 @@ class JsonLog
     end
   end
 
-  def self.get_timestamp(str)
+  def get_timestamp(str)
     if str.index("ERROR")
       substring = "ERROR"
     elsif str.index("WARN")

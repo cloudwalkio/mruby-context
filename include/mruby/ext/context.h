c8eb53cf82b252470c24cb6abd77ef6d20132491
Maurício Spinardi <mauricio.spinardi@cloudwalk.io>
2020-12-10 10:38:58 -0300

    Add conditional support for logcat debugging through custom library

--- include/mruby/ext/context.h
+++ include/mruby/ext/context.h
@@ -11,16 +11,32 @@
 #ifndef _CONTEXT_H_INCLUDED_
 #define _CONTEXT_H_INCLUDED_
 
+#ifdef _ENG_DEBUG_ /* TODO: remove?! */
+#include "libeng/engine.h"
+#endif /* #ifdef _ENG_DEBUG_ */
+
+#if defined ENG_TRACE && !defined TRACE
+#define TRACE(...) ENG_TRACE(__VA_ARGS__)
+#else
 #ifndef TRACE
 #define TRACE(...)
 #endif /* #ifndef TRACE */
+#endif /* #if defined ENG_TRACE && !defined TRACE */
 
+#if defined ENG_TRACE_FUNCTION && !defined TRACE_FUNCTION
+#define TRACE_FUNCTION(...) ENG_TRACE_FUNCTION(__VA_ARGS__)
+#else
 #ifndef TRACE_FUNCTION
 #define TRACE_FUNCTION(...)
 #endif /* #ifndef TRACE_FUNCTION */
+#endif /* #if defined ENG_TRACE_FUNCTION && !defined TRACE_FUNCTION */
 
+#if defined ENG_TRACE_INIT && !defined TRACE_INIT
+#define TRACE_INIT(...) ENG_TRACE_INIT(__VA_ARGS__)
+#else
 #ifndef TRACE_INIT
 #define TRACE_INIT(...)
 #endif /* #ifndef TRACE_INIT */
+#endif /* #if defined ENG_TRACE_INIT && !defined TRACE_INIT */
 
 #endif /* _CONTEXT_H_INCLUDED_ */
